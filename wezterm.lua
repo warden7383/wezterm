41ca416de88d758eb725d1befb38c12e5a078cd5
__warden <NgAndrew123@outlook.com>
2024-02-16 02:36:46 -0800

    feat: background img

--- wezterm.lua
+++ wezterm.lua
@@ -2,8 +2,19 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.font = wezterm.font('JetBrainsMonoNL Nerd Font')
-config.color_scheme_dirs = { '$HOME/.config/wezterm/colors/tokyonight_night'}
+config.color_scheme_dirs = { 'colors/tokyonight_night'}
 config.color_scheme = 'tokyonight_night'
 
+-- config.window_background_gradient = {
+--   colors = {
+--     '#900000'
+--   }
+-- }
 
+config.window_background_image = 'C:/Users/Andrew Ng/.config/wezterm/background/Hell\'s_Paradise_Jigokuraku_Sagiri.jpg'
+config.window_background_image_hsb = {
+  brightness = 0.1,
+  hue = 1.0,
+  saturation = 1.0,
+}
 return config
