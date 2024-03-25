7192ff7c7820f9b843e5be2d4896a1bc9c43c386
__warden <NgAndrew123@outlook.com>
2024-03-25 15:15:57 -0700

    feat: fonts

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,11 @@
 local wezterm = require("wezterm")
 local config = {}
-config.default_prog = { 'pwsh.exe'  }
-config.font = wezterm.font 'JetBrainsMonoNL Nerd Font'
-config.color_scheme_dirs = { 'colors/tokyonight_night'}
+config.default_prog = { 'pwsh.exe' }
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "DemiBold", stretch = "Normal", style = "Normal" }) -- C:\USERS\ANDREW NG\APPDATA\LOCAL\MICROSOFT\WINDOWS\FONTS\JETBRAINSMONONLNERDFONT-REGULAR.TTF, DirectWrite
+config.font_size = 10.0
+config.line_height = 0.9
+config.color_scheme_dirs = { 'colors/tokyonight_night' }
 config.color_scheme = 'tokyonight_night'
-
 wezterm.log_info "HIIII"
 print("testing")
 -- config.window_background_gradient = {
@@ -16,11 +17,16 @@ print("testing")
 config.window_background_image = 'C:/Users/Andrew Ng/.config/wezterm/background/Hell\'s_Paradise_Jigokuraku_Sagiri.jpg'
 config.window_background_image_hsb = {
   brightness = 0.4,
---   -- hue = 1,
---   -- saturation = 0.8,
+  --   -- hue = 1,
+  --   -- saturation = 0.8,
 }
 -- config.source = {
 --   color = "blue"
 -- }
 
+config.window_frame = {
+  font = wezterm.font("Comic Sans MS", { weight = "DemiBold", stretch = "Normal", style = "Normal" }) -- C:\WINDOWS\FONTS\COMIC.TTF, DirectWrite
+}
+config.front_end = "WebGpu"
+config.webgpu_power_preference = "HighPerformance"
 return config
