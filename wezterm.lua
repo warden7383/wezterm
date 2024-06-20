23a20d4c274105c53a29d94168dc206ec80a1541
__warden <NgAndrew123@outlook.com>
2024-06-19 18:47:39 -0700

    feat: used wezterm/rust's triple target to conditionalize config options for specific os' (windows mac linux etc)

--- wezterm.lua
+++ wezterm.lua
@@ -1,25 +1,50 @@
 local wezterm = require("wezterm")
 local config = {}
-config.default_prog = { 'pwsh.exe' }
-config.font = wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" }) -- C:\USERS\ANDREW NG\APPDATA\LOCAL\MICROSOFT\WINDOWS\FONTS\JETBRAINSMONONLNERDFONT-REGULAR.TTF, DirectWrite
-config.font_size = 10.0
-config.line_height = 0.9
+
 config.color_scheme_dirs = { 'colors/tokyonight_night' }
 config.color_scheme = 'tokyonight_night'
 
-
-config.background = {
-  {
-    source = { File = wezterm.config_dir .. '/background/Hell\'s_Paradise_Jigokuraku_Sagiri.jpg' },
-  },
-  {
-    source = { Color = '#141524' }, -- #191a2e #131624 #1a1b26 (default tokyonight_night background)
-    height = '100%',
-    width = '100%',
-    opacity = 0.92,
-  },
-}
-
+if wezterm.target_triple == "aarch64-apple-darwin" then
+  print("im on mac")
+  config.font = wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "DemiBold", stretch = "Normal", style = "Normal" })
+  config.font_size = 13.0
+  config.line_height = 0.9
+  config.background = {
+    {
+      source = { File = wezterm.config_dir .. "/background/Hell's_Paradise_Jigokuraku_Sagiri.jpg" },
+    },
+    {
+      source = { Color = "#141524" }, -- #191a2e #131624 #1a1b26 (default tokyonight_night background)
+      height = "100%",
+      width = "100%",
+      opacity = 0.85,
+    },
+  }
+  -- NOTE: might need to move this elsewhere?
+  config.window_frame = {
+    font_size = 14.0
+  }
+elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  print("im on windows")
+  config.default_prog = { 'pwsh.exe' }
+  config.font = wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" }) -- C:\USERS\ANDREW NG\APPDATA\LOCAL\MICROSOFT\WINDOWS\FONTS\JETBRAINSMONONLNERDFONT-REGULAR.TTF, DirectWrite
+  config.font_size = 10.0
+  config.line_height = 0.9
+  config.background = {
+    {
+      source = { File = wezterm.config_dir .. '/background/Hell\'s_Paradise_Jigokuraku_Sagiri.jpg' },
+    },
+    {
+      source = { Color = '#141524' }, -- #191a2e #131624 #1a1b26 (default tokyonight_night background)
+      height = '100%',
+      width = '100%',
+      opacity = 0.92,
+    },
+  }
+else
+  print("im on linux")
+  print("wezterm is not configured for linux")
+end
 
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.integrated_title_button_style = "Windows"
@@ -27,6 +52,7 @@ config.integrated_title_button_color = "red"
 config.max_fps = 255 -- > 255 max fps returns a table error (at least on windows 11)
 -- config.allow_square_glyphs_to_overflow_width = "Always"
 config.custom_block_glyphs = false
+config.animation_fps = 1
 
 --Tab bar
 config.window_frame = {
@@ -52,7 +78,6 @@ config.window_padding = {
   top = 0,
   bottom = 0,
 }
-
 
 config.front_end = "WebGpu"
 config.webgpu_power_preference = "HighPerformance"
