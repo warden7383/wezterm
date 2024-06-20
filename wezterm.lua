0f1a1f372efe74225917bef9e51e25dc1cafa380
Andrew Ng <NgAndrew123@outlook.com>
2024-06-19 19:26:57 -0700

    bug: window_frame font_size property not working on macOS?

--- wezterm.lua
+++ wezterm.lua
@@ -1,49 +1,51 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme_dirs = { 'colors/tokyonight_night' }
-config.color_scheme = 'tokyonight_night'
+config.color_scheme_dirs = { "colors/tokyonight_night" }
+config.color_scheme = "tokyonight_night"
 
 if wezterm.target_triple == "aarch64-apple-darwin" then
-  print("im on mac")
-  config.font = wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "DemiBold", stretch = "Normal", style = "Normal" })
-  config.font_size = 13.0
-  config.line_height = 0.9
-  config.background = {
-    {
-      source = { File = wezterm.config_dir .. "/background/Hell's_Paradise_Jigokuraku_Sagiri.jpg" },
-    },
-    {
-      source = { Color = "#141524" }, -- #191a2e #131624 #1a1b26 (default tokyonight_night background)
-      height = "100%",
-      width = "100%",
-      opacity = 0.85,
-    },
-  }
-  -- NOTE: might need to move this elsewhere?
-  config.window_frame = {
-    font_size = 14.0
-  }
+	print("im on mac")
+	config.font =
+		wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "DemiBold", stretch = "Normal", style = "Normal" })
+	config.font_size = 13.0
+	config.line_height = 0.9
+	config.background = {
+		{
+			source = { File = wezterm.config_dir .. "/background/Hell's_Paradise_Jigokuraku_Sagiri.jpg" },
+		},
+		{
+			source = { Color = "#141524" }, -- #191a2e #131624 #1a1b26 (default tokyonight_night background)
+			height = "100%",
+			width = "100%",
+			opacity = 0.85,
+		},
+	}
+	-- NOTE: might need to move this elsewhere?
+	config.window_frame = {
+		font_size = 14.0,
+	}
 elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
-  print("im on windows")
-  config.default_prog = { 'pwsh.exe' }
-  config.font = wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" }) -- C:\USERS\ANDREW NG\APPDATA\LOCAL\MICROSOFT\WINDOWS\FONTS\JETBRAINSMONONLNERDFONT-REGULAR.TTF, DirectWrite
-  config.font_size = 10.0
-  config.line_height = 0.9
-  config.background = {
-    {
-      source = { File = wezterm.config_dir .. '/background/Hell\'s_Paradise_Jigokuraku_Sagiri.jpg' },
-    },
-    {
-      source = { Color = '#141524' }, -- #191a2e #131624 #1a1b26 (default tokyonight_night background)
-      height = '100%',
-      width = '100%',
-      opacity = 0.92,
-    },
-  }
+	print("im on windows")
+	config.default_prog = { "pwsh.exe" }
+	config.font =
+		wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" }) -- C:\USERS\ANDREW NG\APPDATA\LOCAL\MICROSOFT\WINDOWS\FONTS\JETBRAINSMONONLNERDFONT-REGULAR.TTF, DirectWrite
+	config.font_size = 10.0
+	config.line_height = 0.9
+	config.background = {
+		{
+			source = { File = wezterm.config_dir .. "/background/Hell's_Paradise_Jigokuraku_Sagiri.jpg" },
+		},
+		{
+			source = { Color = "#141524" }, -- #191a2e #131624 #1a1b26 (default tokyonight_night background)
+			height = "100%",
+			width = "100%",
+			opacity = 0.92,
+		},
+	}
 else
-  print("im on linux")
-  print("wezterm is not configured for linux")
+	print("im on linux")
+	print("wezterm is not configured for linux")
 end
 
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
@@ -56,15 +58,15 @@ config.animation_fps = 1
 
 --Tab bar
 config.window_frame = {
-  font = wezterm.font("Comic Sans MS", { weight = "DemiBold", stretch = "Normal", style = "Normal" }) -- C:\WINDOWS\FONTS\COMIC.TTF, DirectWrite
+	font = wezterm.font("Comic Sans MS", { weight = "DemiBold", stretch = "Normal", style = "Normal" }), -- C:\WINDOWS\FONTS\COMIC.TTF, DirectWrite
 }
 
 config.show_tab_index_in_tab_bar = false
 config.tab_bar_at_bottom = true
 --FROM OFFICIAL DOCS
-wezterm.on('update-right-status', function(window, pane)
-  window:set_left_status '[Left Component] '
-  window:set_right_status '[Right Component] '
+wezterm.on("update-right-status", function(window, pane)
+	window:set_left_status("[Left Component] ")
+	window:set_right_status("[Right Component] ")
 end)
 
 config.use_fancy_tab_bar = false
@@ -73,10 +75,10 @@ config.use_fancy_tab_bar = false
 
 -- window padding
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 config.front_end = "WebGpu"
