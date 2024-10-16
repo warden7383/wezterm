c5ee2c7e2eff6d420467495aa0c2152920586f72
__warden <NgAndrew123@outlook.com>
2024-10-15 22:12:14 -0700

    feat: added background image and removed smartsplits.wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -4,21 +4,21 @@ local keymaps = require("keymaps")
 
 -- local config = {}
 local config = wezterm.config_builder()
-local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
-
-smart_splits.apply_to_config(config, {
-	-- the default config is here, if you'd like to use the default keys,
-	-- you can omit this configuration table parameter and just use
-	-- smart_splits.apply_to_config(config)
-
-	-- directional keys to use in order of: left, down, up, right
-	direction_keys = { "h", "j", "k", "l" },
-	-- modifier keys to combine with direction_keys
-	modifiers = {
-		move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
-		resize = "META", -- modifier to use for pane resize, e.g. META+h to resize to the left
-	},
-})
+-- local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+--
+-- smart_splits.apply_to_config(config, {
+-- 	-- the default config is here, if you'd like to use the default keys,
+-- 	-- you can omit this configuration table parameter and just use
+-- 	-- smart_splits.apply_to_config(config)
+--
+-- 	-- directional keys to use in order of: left, down, up, right
+-- 	direction_keys = { "h", "j", "k", "l" },
+-- 	-- modifier keys to combine with direction_keys
+-- 	modifiers = {
+-- 		move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
+-- 		resize = "META", -- modifier to use for pane resize, e.g. META+h to resize to the left
+-- 	},
+-- })
 
 config.color_scheme_dirs = { "colors/tokyonight_night" }
 config.color_scheme = "tokyonight_night"
@@ -63,9 +63,9 @@ else
 end
 
 config.leader = {
-  key = ";",
-  mods = "ALT",
-  timeout_miliseconds = 1000
+	key = ";",
+	mods = "ALT",
+	timeout_miliseconds = 1000,
 }
 
 config.disable_default_key_bindings = true
