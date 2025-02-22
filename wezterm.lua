a0d47b014df09b890712f726c18693eb6e8a69bb
Andrew Ng <NgAndrew123@outlook.com>
2025-02-22 00:48:05 -0800

    feat(background): adjusted opacity for macbook

--- wezterm.lua
+++ wezterm.lua
@@ -45,7 +45,7 @@ if wezterm.target_triple == "aarch64-apple-darwin" then
 			source = { Color = "#141524" }, -- #191a2e #131624 #1a1b26 (default tokyonight_night background)
 			height = "100%",
 			width = "100%",
-			opacity = 0.85,
+			opacity = 0.98,
 		},
 	}
 elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
